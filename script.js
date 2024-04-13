8bb1146484c766e7c0f552873838eed08cceb466
DGRITS <grizay@icloud.com>
2024-04-13 19:46:35 +0300

    all tasks complete

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 `use strict`;
 
 const http = require('http');
+let firstPageCounter = 0;
+let secondPageCounter = 0;
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
+    firstPageCounter++;
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.end('<h1>Home page</h1>');
+    res.end(`<h1>Корневая страница</h1>
+    <h2>Количество переходов на страницу : ${firstPageCounter}</h2>
+    <a href="/about">Ссылкаа на страницу About</a>`);
   } else if (req.url === '/about') {
+    secondPageCounter++;
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.end('<h1>About</h1>');
+    res.end(`<h1>Страница About</h1>
+    <h2>Количество переходов на страницу : ${secondPageCounter}</h2>
+    <a href="/">Ссылка на главную страницу</a>`);
   } else {
     res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
     res.end('<h1>Erorr page</h1>');
